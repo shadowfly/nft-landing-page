bfd0593510734c2eafec0aa642f343286f7725ca
shadowfly <sit201415@163.com>
2023-11-26 11:05:57 +0800

    Add files via upload

--- src/component/secThree/index.jsx
+++ src/component/secThree/index.jsx
@@ -18,15 +18,15 @@ function SecThree() {
       <div className={styles.TopMargin}>
         <Scroll/>
       
-      <TreeSections date={`April 9 th`} head={`Product Launch`} text={` Our website, socials and mint page will be up at this point so the community can interact with us and see what we're about.Inital minting of 200 NFTs for giveaways, and rewards!`} />
+      <TreeSections date={`December-January 2023`} head={`Protocol Core Components Construction`} text={` Build the core components of the protocol, refine core mechanisms, and enhance computational logic.`} />
       
-      <TreeSectionsTwo date={`April 23 rd`} head={`Owner Vote`} text={`Here we will work out the details on how many validators our community will be setting up as well as how often our owners would like to see a payout.We will have validators on at least 1 network by end of day April 23rd.`} />
+      <TreeSectionsTwo date={`February-March 2024`} head={`Mainnet Testing Phase`} text={`Launch the testnet and implement the economic model.`} />
 
-      <TreeSections date={`April 30 th`} head={`First payout`} text={`One option for our first payout, based on if our owners want to go with weekly payouts, biweekly payouts, or monthly payputs.`} />
+      <TreeSections date={`March-April 2024`} head={`Introduction of WBRT-BIP Protocol`} text={`Release the WBRT-BIP protocol, advance interoperability, and enhance native protocol incentives.`} />
 
-      <TreeSectionsTwo date={`June 1 st`} head={`First Owner Evaluation`} text={`After a little over a month of our validator nodes being online we will be hosting a live chat with owners in our discord to discuss the direction we want to head in. More Validators, more payouts, and an oppurtunity for owners to discuss with us about other projects on chain they would like to see us get involved in. Owner Evalutions will take place every three months after our first one on June 1st.`} />
+      <TreeSectionsTwo date={`May 2024`} head={`SWAP Infrastructure Construction`} text={`Build the foundational infrastructure for the SWAP platform.`} />
       
-      <TreeSections date={`April 30 2023`} head={`Yield Change`} text={`After 1 full year of our project being launched we will host a live chat with Owners about adjusting yield payouts.(The intial yield payout percentage is 50% with 45% being restaked with our validator nodes. The finial 5% will be seperated into 4 sections:1.5% for Maintaining the project,1% in reserve for any costs we may face as Node Trees while defending our project, 0.5% for our marketing team and 0.5% for our Founder, 1.5% for offchain projects at the descreation of the Owners.)`}  />
+                  <TreeSections date={`June 2024`} head={`DeFi Protocol - Lending Products Construction`} text={`Build DeFi protocols and introduce lending products.`}  />
       </div>
       </div>
     </div>
